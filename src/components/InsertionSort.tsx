6c911f713e42ca6f361204bf9cb55d81b204891e
Natália Satie <naty.odashima@gmail.com>
2022-07-06 21:03:00 -0300

    Closes #1

--- src/components/InsertionSort.tsx
+++ src/components/InsertionSort.tsx
@@ -1,32 +1,36 @@
 import * as React from 'react';
-import {ChangeEvent, useState}  from 'react';
+import {FormEvent, useState}  from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import DirectionsIcon from '@mui/icons-material/Directions';
 
 function InsertionSort() {
-  const [arr, setArr] = useState<String>('')
+  const [arr, setArr] = useState<Array<Number>>([])
+  const [input, setInput] = useState<String>('')
 
-  function handleChange(event: ChangeEvent<HTMLInputElement>) {
-    setArr(event.target.value)
+  function strToArray(str: String){
+    return str.split(",").map( elem => {
+      return parseInt(elem, 10)
+    })
   }
 
   return (
     <div>
-      <Box
-        component="form"
-        sx={{
-          '& > :not(style)': { m: 1, width: '25ch' },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField
-          id="filled-basic"
-          onChange={handleChange}
-          label="Lista de números"
-          variant="filled"
-        />
-      </Box>
+      <TextField id="filled-basic" label="Filled" variant="filled" onChange={(e) => setInput(e.target.value)}/>
+      <IconButton type="submit" sx={{ p: '10px' }} aria-label="search" onClick={() => {
+        let res = strToArray(input)
+        setArr(res)
+        console.log(input)
+        console.log(res)
+      }}>
+        <SearchIcon />
+      </IconButton>
     </div>
   );
 }
